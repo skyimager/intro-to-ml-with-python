08f37b1e6201f67088cefd9195cd9ce72cd63886
pradip.gupta <pradip.gupta@ril.com>
2019-12-15 13:55:15 +0530

    week8

---
+++ week8/model_in_production.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify,request
+from sklearn.externals import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def home():
+    return "Success"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_ = request.get_json(silent=True)
+#     print(json_)
+    message=json_.get('message')
+#     print(message)
+    mydf = pd.DataFrame({'message':message})
+#     print(mydf)
+    prediction = clf.predict_proba(mydf['message'])
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    mymodel=open('my_model_pipeline.pkl','rb')
+    clf=joblib.load(mymodel)
+    app.run(port=5000)
